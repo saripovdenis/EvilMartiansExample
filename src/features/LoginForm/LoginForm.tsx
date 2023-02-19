b6673cc5285824d72dfee5e4ab4df3b71b201dd0
Denis Saripov <iam.saripov.denis@gmail.com>
2023-02-20 01:51:42 +0400

    feat: add button styles

--- src/features/LoginForm/LoginForm.tsx
+++ src/features/LoginForm/LoginForm.tsx
@@ -1,17 +1,21 @@
-import React, { FC } from "react";
+import React, { FC, FormEvent } from "react";
 
 import { Input, Button } from "@shared/ui";
 
 import styles from "./LoginForm.module.scss";
 
 export const LoginForm: FC = () => {
+  const handleSubmitFn = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
   return (
     <div className={styles.root}>
-      <form className={styles.form}>
-        <h1 className={styles.header}>Login</h1>
+      <form className={styles.form} onSubmit={handleSubmitFn}>
+        <h1 className={styles.header}>Sign in</h1>
         <Input />
         <Input />
-        <Button text="login" />
+        <Button text="login" className={styles.button} />
       </form>
     </div>
   );
--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,9 +1,17 @@
 import React, { FC } from "react";
+import cx from "classnames";
+
+import styles from "./Button.module.scss";
 
 type Props = {
   text: string;
+  className?: string;
 };
 
-export const Button: FC<Props> = ({ text }) => {
-  return <button>{text}</button>;
+export const Button: FC<Props> = ({ text, className }) => {
+  return (
+    <button type="button" className={cx(styles.root, className)}>
+      {text}
+    </button>
+  );
 };
