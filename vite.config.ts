d853314a3d7845e42d796bb76de3ed0864a1f351
Denis Saripov <iam.saripov.denis@gmail.com>
2023-02-20 05:44:58 +0400

    feat: add gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/EvilMartiansExample/",
   resolve: {
     alias: {
       "@pages": path.resolve(__dirname, "./src/pages"),
