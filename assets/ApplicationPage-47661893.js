69e8d597284abcd09edfcdd92728560a75a150ac
Denis Saripov <iam.saripov.denis@gmail.com>
2023-07-30 03:31:42 +0300

    Updates

---
+++ assets/ApplicationPage-47661893.js
@@ -0,0 +1,1 @@
+import{j as a}from"./index-b0d6768c.js";const t=()=>a("main",{children:"Congratulations!"});export{t as ApplicationPage,t as default};
--- assets/ApplicationPage-f008dab0.js
+++
@@ -1,1 +0,0 @@
-import{j as a}from"./index-f85f8026.js";const t=()=>a("main",{children:"Congratulations!"});export{t as ApplicationPage,t as default};
