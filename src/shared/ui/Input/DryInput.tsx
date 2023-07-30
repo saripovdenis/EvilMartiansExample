3748a97fe976e0e60841cf2cc412be8a17e73626
Denis Saripov <iam.saripov.denis@gmail.com>
2023-07-30 03:08:19 +0300

    fix: clear logs

--- src/shared/ui/Input/DryInput.tsx
+++ src/shared/ui/Input/DryInput.tsx
@@ -5,7 +5,6 @@ import styles from "./DryInput.module.scss";
 type Props = React.InputHTMLAttributes<HTMLInputElement>;
 
 const DryInput = forwardRef<HTMLInputElement, Props>((props, ref) => {
-  console.log(props);
   return (
     <div className={styles.root}>
       <input {...props} className={styles.input} ref={ref} />
